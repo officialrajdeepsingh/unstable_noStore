5817e272f9dfa4cd095e0b92be25586447caaba6
Rajdeep Singh <radewala.2@gmail.com>
2023-12-06 14:18:28 +0530

    redesign UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,103 @@
-import Image from 'next/image'
+import Link from 'next/link';
+import { UsingNoStore } from "@/components/using-nostore"
+import { WithOutNoStore } from '@/components/without-nostore';
+import { UsingNoStoreWithClent } from '@/components/using-nostore-with-client';
 
-export default function Home() {
+interface Post {
+  id: number;
+  title: number;
+  body: string,
+  userId: number,
+  tags: string[],
+  reactions: number
+}
+
+interface ReturnData {
+  posts: Post[]
+  total: number;
+  skip: number;
+  limit: number;
+}
+
+// static 
+async function getFetchData(): Promise<ReturnData> {
+  let getData = await fetch('https://dummyjson.com/posts?limit=10')
+  return getData.json()
+}
+
+
+async function getRandomNumberWithNoStore(): Promise<{ random: number }> {
+  let getRandomData = await fetch('http://localhost:3000/api/random')
+  return getRandomData.json()
+}
+
+async function getRandomNumber(): Promise<{ random: number }> {
+
+  let getRandomData = await fetch('http://localhost:3000/api/random')
+
+  return getRandomData.json()
+}
+
+export default async function Home() {
+  // noStore();
+
+  let { posts } = await getFetchData();
+
+  let { random } = await getRandomNumberWithNoStore();
+
+  let getRandom = await getRandomNumber()
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col items-center justify-between">
+
+      <div className="my-12 mb-8">
+        <button className=" text-md md:text-6xl text-center"> No Store. </button>
+      </div>
+
+      <h2 className='mb-5 text-5xl'> using NoStore</h2>
+
+      <UsingNoStore />
+
+      <h2 className=' my-8 text-5xl'> using NoStore with client component</h2>
+
+      <UsingNoStoreWithClent />
+
+      <h2 className='mt-8 text-5xl'> without  NoStore</h2>
+
+      <div className="mt-6 grid grid-cols-2 gap-4 md:grid-cols-2 lg:gap-8">
+
+        <div className="flex flex-col items-center justify-center rounded-lg bg-gray-300 p-4 lg:p-8">
+          <div className="text-xl font-bold text-indigo-500 sm:text-2xl md:text-3xl">{random}</div>
         </div>
+
+        <div className="flex flex-col items-center justify-center rounded-lg bg-gray-300 p-4 md:p-8">
+          <div className="text-xl font-bold text-indigo-500 sm:text-2xl md:text-3xl">{getRandom.random}</div>
+        </div>
+
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <h2 className='mt-24 mb-10 text-5xl'>All Posts</h2>
+      <div className="block w-10/12 sm:w-8/12 md:w-7/12  lg:w-6/12 mx-auto">
+        {
+          posts.map((post: Post) => {
+            let getKey = post.userId * post.id
+            return (
+              <div key={getKey} className="my-14 p-8 md:p-16 bg-white rounded-lg dark:bg-gray-800">
+                <h2 className="mb-3 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">{post.title}</h2>
+                <p className="mb-3 text-gray-500 dark:text-gray-400"> {post.body} </p>
+                <Link href="/" className="inline-flex items-center font-medium text-blue-600 hover:text-blue-800 dark:text-blue-500 dark:hover:text-blue-700">
+                  Learn more
+                  <svg className=" w-2.5 h-2.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
+                  </svg>
+                </Link>
+              </div>
+            )
+          })
+        }
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
+
 }
